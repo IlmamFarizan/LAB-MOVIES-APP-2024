fc1ad83f426990ee58890786d46605e041cf95fb
IlmamFarizan <ilmam.farizan@yahoo.com>
2024-03-29 00:15:19 +0800

    fixed the error

--- src/components/movieList/index.tsx
+++ src/components/movieList/index.tsx
@@ -4,14 +4,15 @@ import Grid from "@mui/material/Grid";
 import { BaseMovieList } from "../../types/interfaces";
 
 interface MovieListProps extends BaseMovieList {
+  selectFavourite: (movieId: number) => void;
   addToFavourites: (movieId: number) => void;
 }
 
-const MovieList: React.FC<BaseMovieList> = (props) => {
+const MovieList: React.FC<MovieListProps> = (props) => {
   const movies = props.movies;
   let movieCards = movies.map((m) => (
     <Grid key={m.id} item xs={12} sm={6} md={4} lg={3} xl={2}>
-      <Movie key={m.id} movie={m} selectFavourite={props.selectFavourite} />
+      <Movie key={m.id} {...m} selectFavourite={props.selectFavourite} />
     </Grid>
   ));
   return movieCards;
