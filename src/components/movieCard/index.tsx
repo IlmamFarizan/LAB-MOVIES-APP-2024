29f51faecd47a1e4a4dc9fc0ed8dcaa4d1abfb15
IlmamFarizan <ilmam.farizan@yahoo.com>
2024-04-30 23:54:00 +0800

    Refactor: Favourite movie list now stored in a context.

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from "react";
+import React, { MouseEvent, useContext } from "react";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -15,6 +15,8 @@ import img from "../../images/film-poster-placeholder.png";
 import { BaseMovie } from "../../types/interfaces";
 import { Link } from "react-router-dom";
 import Avatar from "@mui/material/Avatar";
+import { MoviesContext } from "../../contexts/moviesContext";
+import { ListedMovie } from "../../types/interfaces";
 
 const styles = {
   card: { maxWidth: 345 },
@@ -28,17 +30,22 @@ interface MovieCardProps extends BaseMovie {
   selectFavourite: (movieId: number) => void;
 }
 
-const MovieCard: React.FC<MovieCardProps> = (props) => {
+const MovieCard: React.FC<ListedMovie> = (props) => {
+  const movie = { ...props, favourite: false };
+  const { favourites, addToFavourites } = useContext(MoviesContext);
+
+  if (favourites.find((id) => id === movie.id)) movie.favourite = true;
+
   const handleAddToFavourite = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    props.selectFavourite(props.id);
+    addToFavourites(movie);
   };
 
   return (
     <Card sx={styles.card}>
       <CardHeader
         avatar={
-          props.favourite ? (
+          movie.favourite ? (
             <Avatar sx={styles.avatar}>
               <FavoriteIcon />
             </Avatar>
@@ -46,15 +53,15 @@ const MovieCard: React.FC<MovieCardProps> = (props) => {
         }
         title={
           <Typography variant="h5" component="p">
-            {props.title}{" "}
+            {movie.title}{" "}
           </Typography>
         }
       />
       <CardMedia
         sx={styles.media}
         image={
-          props.poster_path
-            ? `https://image.tmdb.org/t/p/w500/${props.poster_path}`
+          movie.poster_path
+            ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
             : img
         }
       />
@@ -63,13 +70,13 @@ const MovieCard: React.FC<MovieCardProps> = (props) => {
           <Grid item xs={6}>
             <Typography variant="h6" component="p">
               <CalendarIcon fontSize="small" />
-              {props.release_date}
+              {movie.release_date}
             </Typography>
           </Grid>
           <Grid item xs={6}>
             <Typography variant="h6" component="p">
               <StarRateIcon fontSize="small" />
-              {"  "} {props.vote_average}{" "}
+              {"  "} {movie.vote_average}{" "}
             </Typography>
           </Grid>
         </Grid>
@@ -81,7 +88,7 @@ const MovieCard: React.FC<MovieCardProps> = (props) => {
         >
           <FavoriteIcon color="primary" fontSize="large" />
         </IconButton>
-        <Link to={`/movies/${props.id}`}>
+        <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
           </Button>
--- src/index.tsx
+++ src/index.tsx
@@ -9,30 +9,36 @@ import SiteHeader from "./components/siteHeader";
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 360000,
-      refetchInterval: 360000,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+import MoviesContextProvider from "./contexts/moviesContext";
 
 const App = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 360000,
+        refetchInterval: 360000,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <SiteHeader />
-        <Routes>
-          <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
-          <Route path="/reviews/:id" element={<MovieReviewPage />} />
-          <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
-          <Route path="/movies/:id" element={<MoviePage />} />
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
+        <MoviesContextProvider>
+          <Routes>
+            <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route path="/reviews/:id" element={<MovieReviewPage />} />
+            <Route
+              path="/movies/favourites"
+              element={<FavouriteMoviesPage />}
+            />
+            <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </MoviesContextProvider>
       </BrowserRouter>
     </QueryClientProvider>
   );
