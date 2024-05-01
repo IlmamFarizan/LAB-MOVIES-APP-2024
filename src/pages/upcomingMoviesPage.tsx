792a96d9256b9ff50cf9669c99a03a161bab83f7
IlmamFarizan <ilmam.farizan@yahoo.com>
2024-05-01 23:36:21 +0800

    Add new review support.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { ListedMovie } from "../types/interfaces";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
 
 const styles = {
   root: {
@@ -38,7 +39,9 @@ const UpcomingMoviesPage: React.FC = () => {
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      selectFavourite={addToFavourites}
+      action={(movie: ListedMovie) => {
+        return <AddToFavouritesIcon {...movie} />;
+      }}
     />
   );
 };
